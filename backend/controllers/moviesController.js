a0b578d49e394f64476208e98d007a8c21cc70d4
Nishant Satish <nishantsatishkumar@gmail.com>
2023-02-17 17:20:47 +0530

    Backend fucntionality done

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -1,35 +1,46 @@
 const movieList = require('../helpers/getMovies');
 const creditsList = require('../helpers/getCredits');
+const Media = require('../models/media');
 
 module.exports = {
   getMovies: async (req, res) => {
-    const m1 = (await movieList(205596)).data;
-    const m2 = (await movieList(388)).data;
-    const m3 = (await movieList(398978)).data;
-    const m4 = (await movieList(1597)).data;
+    const m1 = (await movieList(205596)).response.data;
+    const m2 = (await movieList(388)).response.data;
+    const m3 = (await movieList(398978)).response.data;
+    const m4 = (await movieList(1597)).response.data;
 
     // We only need movie names for the home page
-    const movies = [m1, m2, m3, m4].map((movie) => {
-      return { id: movie.id, name: movie.original_title };
-    });
+    const movies = await Promise.all(
+      [m1, m2, m3, m4].map(async (movie) => {
+        return {
+          id: movie.id,
+          name: movie.original_title,
+          image: (await movieList(movie.id)).image,
+          trailer: (await movieList(movie.id)).trailer,
+        };
+      })
+    );
     res.status(200).json(movies);
   },
 
   getMoviePopupDetails: async (req, res) => {
-    const movie = (await movieList(req.params.id)).data;
+    const movie = (await movieList(req.params.id)).response.data;
+    const image = (await movieList(req.params.id)).image;
     const reqdMovieDetails = {
       id: movie.id,
       name: movie.original_title,
       year: movie.release_date.slice(0, 4),
       duration: movie.runtime,
       description: movie.overview,
+      image,
     };
     res.status(200).json(reqdMovieDetails);
   },
 
   getCompleteMovie: async (req, res) => {
-    const movie = (await movieList(req.params.id)).data;
+    const movie = (await movieList(req.params.id)).response.data;
     const crewMembers = (await creditsList(req.params.id)).data;
+    const trailer = (await movieList(req.params.id)).trailer;
 
     const actors = crewMembers.cast.map((crewMember) => {
       if (crewMember.known_for_department == 'Acting') {
@@ -53,6 +64,7 @@ module.exports = {
       description: movie.overview,
       actors: actors.splice(0, 3),
       director,
+      trailer,
     };
 
     res.status(200).json(reqdMovieDetails);
--- backend/helpers/getMovies.js
+++ backend/helpers/getMovies.js
@@ -1,9 +1,17 @@
 const axios = require('axios').default;
+const Media = require('../models/media');
 
 const getAMovie = async (id) => {
-  return axios.get(
+  const res = await Media.findOne({
+    movie_id: id,
+  });
+  const image = res.image;
+  // console.log(image);
+  const trailer = res.trailer;
+  const response = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=a810233f45bb4767b9a04b931179a085&language=en-US`
   );
+  return { response, trailer, image };
 };
 
 module.exports = getAMovie;
